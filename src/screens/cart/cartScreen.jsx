c398f8fa4c7b40f649ad9e34c6974494371f9ef4
Dipen Khatri <khatridipen7@gmail.com>
2024-05-23 13:01:05 +0545

    Complete: v1

--- src/screens/cart/cartScreen.jsx
+++ src/screens/cart/cartScreen.jsx
@@ -6,21 +6,23 @@ import CustomButton from "../../components/customButton";
 import { Colors } from "../../constants/styles";
 
 /**
- * The CartScreen component displays the products added to the cart.
- * It allows users to adjust the quantity of products and remove them from the cart.
+ * Component for the Cart Screen of the application.
+ * Displays the products added to the cart along with their details.
+ * Users can update the quantity of products or remove them from the cart.
+ * @returns {JSX.Element} - Cart Screen component.
  */
 function CartScreen() {
   const { cart, updateQuantity, removeFromCart } = useCart();
 
-  // Calculate the total amount of the cart
+  // Calculate total amount by multiplying price and quantity for each product in the cart
   const totalAmount = cart.reduce(
     (total, item) => total + item.price * item.quantity,
     0
   );
 
   return (
-    <View style={{ flex: 1, padding: 16, backgroundColor: "white" }}>
-      {/* Render the list of cart items */}
+    <View className="flex-1 p-4 bg-white">
+      {/* Render the list of products in the cart */}
       <FlatList
         data={cart}
         keyExtractor={(item) => item.id.toString()}
@@ -32,33 +34,10 @@ function CartScreen() {
           />
         )}
       />
-
-      {/* Divider */}
-      <View
-        style={{
-          height: 1,
-          width: "100%",
-          backgroundColor: "#D1D5DB",
-          marginVertical: 10,
-        }}
-      />
-
-      {/* Total amount and Checkout button */}
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 20,
-            fontWeight: "bold",
-            textAlign: "center",
-            fontFamily: "urbanist-medium",
-          }}
-        >
+      <View className="h-px w-full bg-gray-400 my-3" />
+      {/* Render total amount and checkout button */}
+      <View className="flex-row justify-between items-center align-center">
+        <Text className="text-xl font-bold text-center font-urbanist-medium">
           Total: ${totalAmount.toFixed(2)}
         </Text>
         <CustomButton
@@ -81,101 +60,55 @@ function CartScreen() {
 }
 
 /**
- * The CartItem component represents an individual item in the cart list.
- * It displays product details, allows adjusting quantity, and removing the product from the cart.
- * @param {Object} product - The product object containing details like id, title, image, price, and quantity.
- * @param {Function} updateQuantity - Function to update the quantity of the product.
- * @param {Function} removeFromCart - Function to remove the product from the cart.
+ * Component for rendering each item in the cart.
+ * @param {object} props - Component props containing product details and functions for updating quantity and removing from cart.
+ * @returns {JSX.Element} - Cart item component.
  */
 function CartItem({ product, updateQuantity, removeFromCart }) {
   return (
-    <View
-      style={{
-        marginBottom: 20,
-        padding: 16,
-        borderWidth: 1,
-        borderColor: "#D1D5DB",
-        borderRadius: 8,
-      }}
-    >
-      <View style={{ flexDirection: "row" }}>
+    <View className="flex-column mb-4 p-4 border border-gray-300 rounded-lg">
+      <View className="flex-row">
         <Image
           source={{ uri: product.image }}
+          className="mr-4"
           style={{
-            marginRight: 12,
             width: 100,
             height: 100,
             resizeMode: "contain",
           }}
         />
-        <View style={{ flex: 1 }}>
-          <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 8 }}>
-            {product.title}
-          </Text>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ fontSize: 16, marginRight: 20 }}>
-              ${product.price.toFixed(2)}
-            </Text>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <View className="flex-1">
+          <Text className="text-lg font-bold mb-2">{product.title}</Text>
+          <View className="flex-row items-center justify-between">
+            <Text className="text-base">${product.price.toFixed(2)}</Text>
+            <View className="flex-row items-center">
               <Pressable
-                style={{
-                  backgroundColor: "#E5E7EB",
-                  borderRadius: 20,
-                  padding: 8,
-                  marginRight: 8,
-                }}
+                className="bg-gray-200 rounded-full p-2 cursor-pointer mr-2"
                 onPress={() =>
                   updateQuantity(product.id, Math.max(1, product.quantity - 1))
                 }
               >
-                <Ionicons name="remove-outline" size={24} color="black" />
+                <Ionicons name="remove-outline" size={18} color="black" />
               </Pressable>
-              <Text style={{ fontSize: 18, marginHorizontal: 8 }}>
-                {product.quantity}
-              </Text>
+
+              <View className="bg-gray-200 rounded-md p-1 px-2">
+                <Text className="text-lg">{product.quantity}</Text>
+              </View>
+
               <Pressable
-                style={{
-                  backgroundColor: "#E5E7EB",
-                  borderRadius: 20,
-                  padding: 8,
-                  marginLeft: 8,
-                }}
+                className="bg-gray-200 rounded-full p-2 cursor-pointer ml-2"
                 onPress={() => updateQuantity(product.id, product.quantity + 1)}
               >
-                <Ionicons name="add-outline" size={24} color="black" />
+                <Ionicons name="add-outline" size={18} color="black" />
               </Pressable>
             </View>
           </View>
         </View>
       </View>
-      <View
-        style={{
-          height: 1,
-          width: "100%",
-          backgroundColor: "#D1D5DB",
-          marginVertical: 10,
-        }}
-      />
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 16,
-            fontWeight: "bold",
-            fontFamily: "urbanist-medium",
-          }}
-        >
+      <View className="h-px w-full bg-gray-400 my-3" />
+
+      <View className="flex-row items-center justify-between">
+        <Text className="text-base font-bold font-urbanist-medium">
           Total: ${(product.price * product.quantity).toFixed(2)}
         </Text>
         <CustomButton
@@ -192,3 +125,5 @@ function CartItem({ product, updateQuantity, removeFromCart }) {
     </View>
   );
 }
+
+export default CartScreen;
--- src/screens/home/homeScreen.jsx
+++ src/screens/home/homeScreen.jsx
@@ -17,20 +17,23 @@ import ProductItem from "../../components/productItem";
 const screenHeight = Dimensions.get("window").height;
 
 /**
- * Component for the home screen displaying various product categories and available products.
- * Fetches and displays products from the server.
+ * Component for the Home Screen of the application.
+ * Displays best offers, carousel of products, and available products.
+ * Users can navigate to the Search Screen to view all available products.
+ * @param {object} navigation - Navigation object used to navigate between screens.
+ * @returns {JSX.Element} - Home Screen component.
  */
 function HomeScreen({ navigation }) {
-  const [products, setProducts] = useState([]); // State to store fetched products
-  const { addToCart } = useCart(); // Access addToCart function from cart context
-  const [isLoading, setIsLoading] = useState(true); // State to track loading status
+  const [products, setProducts] = useState([]);
+  const { addToCart } = useCart();
+  const [isLoading, setIsLoading] = useState(true);
 
-  // Fetch products from the server when the component mounts
+  // Fetch products from API when the component mounts
   useEffect(() => {
     fetchData();
   }, []);
 
-  // Function to fetch products from the server
+  // Function to fetch products from API
   const fetchData = async () => {
     try {
       const data = await fetchProducts();
@@ -38,27 +41,30 @@ function HomeScreen({ navigation }) {
     } catch (error) {
       console.error(error);
     } finally {
-      setIsLoading(false); // Update loading state after fetching data
+      setIsLoading(false);
     }
   };
 
-  // Render loading indicator while fetching data
+  // Render loading screen while products are being fetched
   if (isLoading) {
     return <Loading message={"Loading products..."} />;
   }
 
   return (
     <ScrollView style={styles.container}>
+      {/* Display best offers */}
       <CategoryText
         leftText="Best Offers"
         imageSourcePrototype={require("../../../assets/images/best_picks.png")}
         onPress={() => {}}
       />
-      <View style={styles.separator} />
+      <View className="h-px w-full bg-gray-400 mb-3" />
+      {/* Display carousel of products */}
       <View style={styles.carouselContainer}>
         <CarouselSlider items={products} />
       </View>
-      <View style={styles.separator} />
+      <View className="h-px w-full bg-gray-400 mt-7 mb-2" />
+      {/* Display available products */}
       <CategoryText
         leftText="Available Products"
         rightText="See all"
@@ -66,8 +72,9 @@ function HomeScreen({ navigation }) {
           navigation.navigate("Search");
         }}
       />
-      <View style={styles.spacing} />
+      <View className="mt-4" />
       <View style={styles.listContainer}>
+        {/* Display available products as a list */}
         <FlatList
           data={products}
           keyExtractor={(item) => item.id.toString()}
@@ -81,23 +88,14 @@ function HomeScreen({ navigation }) {
   );
 }
 
-// Stylesheet for the HomeScreen component
+// Styles for the HomeScreen component
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
   },
-  separator: {
-    height: StyleSheet.hairlineWidth,
-    width: "100%",
-    backgroundColor: "#ccc",
-    marginVertical: 3,
-  },
   carouselContainer: {
     height: screenHeight / 3,
-  },
-  spacing: {
-    marginTop: 4,
   },
   listContainer: {
     flex: 1,
